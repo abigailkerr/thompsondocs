5ed1b13e9a22fd4fd9c22db99594b4575938dfae
abigailkerr <abigail.kerr@ku.edu>
2025-09-03 11:35:01 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_favicon = 'ku.png'
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'TG_logo.png'
 html_theme_options = { 'logo_only': True }
